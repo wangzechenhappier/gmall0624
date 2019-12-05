2136aa6f65481a222864ca6f3f06aa9a1cc4fc76
wangzechenhappier <963064506@qq.com>
2019-12-05 11:39:23 +0800

    1204

---
+++ gmall-user-manage/src/main/java/com/atguigu/gmall0624/user/service/impl/UserInfoServiceImpl.java
@@ -0,0 +1,67 @@
+package com.atguigu.gmall0624.user.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall0624.bean.UserAddress;
+import com.atguigu.gmall0624.bean.UserInfo;
+import com.atguigu.gmall0624.service.UserInfoService;
+import com.atguigu.gmall0624.user.mapper.UserAddressMapper;
+import com.atguigu.gmall0624.user.mapper.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+
+@Service
+public class UserInfoServiceImpl implements UserInfoService {
+
+    // 调用mapper
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    @Autowired
+    private UserAddressMapper userAddressMapper;
+
+    @Override
+    public List<UserInfo> findAll() {
+        return userInfoMapper.selectAll();
+    }
+
+    @Override
+    public List<UserInfo> findUserInfo(UserInfo userInfo) {
+        return null;
+    }
+
+    @Override
+    public List<UserInfo> findByNickName(String nickName) {
+        return null;
+    }
+
+    @Override
+    public void addUser(UserInfo userInfo) {
+
+    }
+
+    @Override
+    public void updUser(UserInfo userInfo) {
+
+    }
+
+    @Override
+    public void delUser(UserInfo userInfo) {
+
+    }
+
+    @Override
+    public List<UserAddress> findUserAddressListByUserId(String userId) {
+        // select * from userAddress where userId = ?
+        // 查询那张表，则就使用哪张表的mapper
+        UserAddress userAddress = new UserAddress();
+        userAddress.setUserId(userId);
+        return userAddressMapper.select(userAddress);
+    }
+
+    @Override
+    public List<UserAddress> findUserAddressListByUserId(UserAddress userAddress) {
+        return userAddressMapper.select(userAddress);
+    }
+}
